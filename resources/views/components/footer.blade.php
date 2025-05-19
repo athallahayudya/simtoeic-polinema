c3b31d108ce7ad7a09cc2f11dcb7370d6c125c2b
athallahayudya <athallah08.6a@gmail.com>
2025-05-19 19:33:37 +0700

    feat(footer): add footer with description and social icons

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,28 @@
 <footer class="main-footer">
-    <div class="footer-left">
-        Copyright &copy; 2025 <div class="bullet"></div> Design By <a href="https://github.com/yonandamayla/simtoeic-polinema">PBL Group 2</a>
-    </div>
-    <div class="footer-right">
-        2.3.0
+    <div class="container">
+        <div class="footer-left">
+            <!-- Logo and Description -->
+            <div class="footer-section">
+                <h3 class="footer-logo">SIMTOEIC-POLINEMA</h3>
+                <p class="footer-text">
+                    No. 9 Soekarno Hatta St., Jatimulyo, Lowokwaru District, Malang City, East Java 65141
+                </p>
+            </div>
+        </div>
+        <div class="footer-right">
+            <div class="footer-section">
+                <div class="social-icon">
+                    <a href="#"><i class="fab fa-twitter" style="font-size: 20px;"></i></a>
+                    <a href="#"><i class="fab fa-facebook-f" style="font-size: 20px;"></i></a>
+                    <a href="https://www.instagram.com/upabahasa"><i class="fab fa-instagram" style="font-size: 20px;"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copyright -->
+        <div class="footer-copyright">
+            Copyright &copy; 2025 &nbsp; • &nbsp; Design By
+            <a href="https://github.com/yonandamayla/simtoeic-polinema">PBL Group 2</a>
+        </div>
     </div>
 </footer>
