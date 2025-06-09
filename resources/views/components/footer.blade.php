d76eb299324569f763a6902d30807b9f076df8e0
Audric Andhika <audricc015@gmail.com>
2025-06-09 18:05:16 +0700

    refactor: resize footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,29 +1,26 @@
 <footer class="main-footer">
     <div class="container">
-        <div class="footer-left">
-            <!-- Logo + Alamat -->
-            <div class="footer-section" style="margin-bottom: 10px;">
-                <img src="{{ asset('img/simtoeic0.png') }}" alt="SIMTOEIC Logo" style="height: 90px; margin-bottom: 10px;">
-                <p class="footer-text" style="margin-top: 10px;">
-                    POLITEKNIK NEGERI MALANG<br>
-                    No. 9 Soekarno Hatta St., Jatimulyo, Lowokwaru District, Malang City, East Java 65141<br>
-                    (0341) 404424
-                </p>
+        <div class="footer-content">
+            <div class="footer-left">
+                <!-- Logo -->
+                <div class="footer-section">
+                    <img src="{{ asset('img/simtoeic0.png') }}" alt="SIMTOEIC Logo" style="height: 70px; vertical-align: middle;">
+                </div>
             </div>
-        </div>
-        <div class="footer-right">
-            <div class="footer-section">
-                <div class="social-icon">
-                    <a href="#"><i class="fab fa-twitter" style="font-size: 20px;"></i></a>
-                    <a href="#"><i class="fab fa-facebook-f" style="font-size: 20px;"></i></a>
-                    <a href="https://www.instagram.com/upabahasa"><i class="fab fa-instagram" style="font-size: 20px;"></i></a>
+            <div class="footer-right">
+                <div class="footer-section">
+                    <div class="social-icon">
+                        <a href="#" class="twitter"><i class="fab fa-twitter" style="font-size: 20px;"></i></a>
+                        <a href="#" class="facebook"><i class="fab fa-facebook-f" style="font-size: 20px;"></i></a>
+                        <a href="https://www.instagram.com/upabahasa" class="instagram"><i class="fab fa-instagram" style="font-size: 20px;"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
 
         <!-- Copyright -->
         <div class="footer-copyright">
-            Copyright &copy; 2025 &nbsp; • &nbsp; Design By
+            Copyright © 2025  •  Design By
             <a href="https://github.com/yonandamayla/simtoeic-polinema">PBL Group 2</a>
         </div>
     </div>
